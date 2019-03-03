382af8f0b4b273d5a2749cc41db13e9180111972
Pooja <poojahagavane@gmail.com>
2019-03-03 14:52:06 -0500

    First commit

---
+++ src/main/java/com/example/Pollswebapp/PollswebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pollswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollswebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollswebappApplication.class, args);
+	}
+
+}
